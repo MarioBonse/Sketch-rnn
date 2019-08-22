Generator implemented and fix bug of the reconstruction loss. TODO understand if I should use layer normalization

src/data_Manager.py
@@ -125,13 +125,18 @@ class DataGenerator(tf.keras.utils.Sequence):
         'Initialization'
         self.Data = Data
         self.validation = validation
-        self.batch_size = HP.batch_size
+        if validation:
+            self.batch_size = len(Data)
+        else:
+            self.batch_size = HP.batch_size
         self.dimention = len(Data)
         self.shuffle = shuffle
         self.on_epoch_end()
 
     def __len__(self):
-        'Denotes the number of batches per epoch'    
+        'Denotes the number of batches per epoch'
+        if self.validation:
+            return 1    
         return int(np.floor(self.dimention/ self.batch_size))
 
     def __getitem__(self, index):
@@ -196,8 +201,14 @@ class changing_KL_wheight(Callback):
     def on_train_batch_end(self, epochs, logs = {}):
         pass
 
-    def on_test_begin(self, epochs, logs = {}):
+    def on_test_batch_begin(self, epochs, logs = {}):
         pass
 
-    def on_test_end(self, epochs, logs = {}):
+    def on_test_batch_end(self, epochs, logs = {}):
+        pass
+    
+    def on_test_begin(self, *arg, **karg):
+        pass
+
+    def on_test_end(self, *arg, **karg):
         pass

src/generate.py
@@ -1,69 +1,139 @@
+import draw
+import data_Manager
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 from HyperParameters import HP
 import train_utility as tu
-"""
-we create only the decoder in order to generate data
-"""
-# the decoder LSTM has an input composed by the sequence and also the hidden state
-decoder_input_sequence = tf.keras.layers.Input(shape = (HP.max_seq_length, HP.input_dimention), name = "decoder_seq_input" )
-# we have also the latent variable as input of the LSTM with the sequence
-latent_variable = tf.keras.layers.Input(shape = (HP.latent_dim), name = "latent_variable" )
-inputLatentVariable = tf.keras.layers.RepeatVector(HP.max_seq_length)(latent_variable)
-# so we concatentate the two vector
-# as input we have the two vector above concatenated
-totalInput = tf.keras.layers.Concatenate()([decoder_input_sequence, inputLatentVariable])
-# Create LSTM for generation with input state = tanh(z)
-decoderLSTM = tf.keras.layers.LSTM(HP.dec_hidden_size, recurrent_dropout=HP.rec_dropout, 
-                                    return_sequences=True, return_state=True, name = "LSTM_decoder")
-#
-init_state = tf.keras.layers.Dense(units=(2*decoderLSTM.units), activation='tanh', name = "decoder_init_stat")(latent_variable)
-h_0, c_0 = tf.split(init_state, num_or_size_splits=2, axis = 1)
-# creation of the LSTM
-decoder_output, _, _ = decoderLSTM(totalInput, initial_state = [h_0, c_0])
+import random
 
-# dense to output. THe dimention is, as explained in the paper equal to 3 + 6*M
-# 6 times M= number of mixture 
-output_dimention = (3 + HP.M * 6)
-distribution_output = tf.keras.layers.Dense(output_dimention, name = "output_layer")(decoder_output)
 
-# Now we load the weights from the trained model
-generator = tf.keras.models.Model([decoder_input_sequence, latent_variable], distribution_output)
-generator.summary()
-generator.load_weights(HP.model_folder+ HP.model_name, by_name = True)
-generator.build(tf.TensorShape([1, None])) 
-generator.summary/()
+def sample_from_categorical(pdf):
+    x = random.random()
+    cum_sum = 0
+    for i, p in enumerate(pdf):
+        cum_sum += p
+        if cum_sum >= x:
+            return i
+    print("Error dureing sampling\n pdf is ")
+    print(pdf)
+    return False
+
+
+def find_distribution_parameter_decoder(output, temperature = 1.):
+    # the raw output has to be divided into the the distribution parameters.
+    # they also have to be normalized
+    # 3 parameters for the pen state 
+    logit = output[:,:,:3]
+    logit = logit/temperature
+    # now we have 6 parameter for each gaussian
+    distribution_parameter = [output[:, :, (3 + HP.M * (n - 1)):(3 + HP.M * n)] for n in range(1, 7)]
+    # now divide them 
+    [pi, mux, muy, sigmax, sigmay, ro] = distribution_parameter
+    pi = pi/ temperature
+    
+    # normalize
+    # sigma > 0 -> exp(sigma)
+    sigmax = np.exp(sigmax)
+    sigmay = np.exp(sigmay)
+    # sigma^2 = sigma^2*temperature but we have sigma 
+    sigmax = sigmax*np.sqrt(temperature)
+    sigmay = sigmay*np.sqrt(temperature)
+    # -1 < ro < 1 -> tanh
+    ro = np.tanh(ro)
+    # logits and pi have to be a convex hull -> softmax
+    logit = np.squeeze(logit)
+    pi = np.squeeze(pi)
+    logit =  np.exp(logit)/sum(np.exp(logit))
+    pi = np.exp(pi)/sum(np.exp(pi))
+    return logit, pi, np.squeeze(mux), np.squeeze(muy), np.squeeze(sigmax), np.squeeze(sigmay), np.squeeze(ro)
 
 """
-funciton that generate point by point the sketch
+function that generate point by point the sketch
 """
-def generate_text(model, latent_varoable, temperature = 1):
+def generate_sketch(decoder, initial_state_net, latent_variable = None, temperature = 1.):
     # Evaluation step (generating text using the learned model)
-    points_generated = []
-    starting_point = np.array([0,0,1,0,0])
+    starting_point = np.zeros((1,5))
+    starting_point[0,2] = 1
     previous_point = starting_point
-
-    model.reset_states()
+    if latent_variable == None:
+        latent_variable = np.random.rand(1,HP.latent_dim)
+    initial_state = initial_state_net.predict(latent_variable)
+    h_0, c_0 = tf.split(initial_state, num_or_size_splits=2, axis = 1)
+    points = []
     for i in range(HP.max_seq_length):
-        new_input = tf.concatenate(previus_point, latent_variable)
-        predictions = model(new_input)
-        # remove the batch dimension
-        predictions = tf.squeeze(predictions, 0)
-
-        q, pi, mux, muy, sigmax, sigmay, ro = tu.find_distribution_parameter(predictions, temperature = temperature)
+        new_input = np.concatenate((previous_point, latent_variable), axis =  1)
+        new_input =  np.expand_dims(new_input, axis=0)
+        predictions = decoder.predict([new_input, h_0, c_0], batch_size = 1, steps = 1)
+    
+        # obtain mixture parameters
+        LSTM_output = predictions[0]
+        
+        # obtain states h_new and c_new
+        h_0 = predictions[1]
+        c_0 = predictions[2]
+        
+        
+        
+        q, pi, mux, muy, sigmax, sigmay, ro = find_distribution_parameter_decoder(LSTM_output, temperature = temperature)     
+        
+        print(q)
+        # sample from the categorical distribution
+        q_index = sample_from_categorical(q)
+        if q_index == 2:
+            print("end of sketch")
+            points.append(np.array([0,0,1,0,0]))
+            return np.array(points)
+        
+        pi_index = sample_from_categorical(pi)
         
         # sample from the bivariate normals
-        covariance_matrix = np.matrix([sigmax**2, ro*sigmax*sigmay], [ ro*sigmax*sigmay, sigmay**2])
-        newdelta = np.random.multivariate_normal(mean = np.array(mux, muy), cov = covariance_matrix)
-        
+        new_sigma_x = sigmax[pi_index]
+        new_sigma_y = sigmay[pi_index]
+        new_ro = ro[pi_index]
+        new_mu_x = mux[pi_index]
+        new_mu_y = muy[pi_index]
+        covariance_matrix = [[new_sigma_x**2, new_ro*new_sigma_x*new_sigma_y],  [ new_ro*new_sigma_x*new_sigma_y, new_sigma_y]]
+        dx, dy = np.random.multivariate_normal(mean = (new_mu_x, new_mu_y), cov = covariance_matrix)
+        new_elem = [dx, dy, 0, 0, 0]
+        new_elem[2+q_index] = 1
+        new_elem =np.array(new_elem)
+        points.append(new_elem)
+        previous_point = np.expand_dims(new_elem, axis = 0)
+    return np.array(points)
 
-        # using the GMM to predict dx, dy and categorical to predice p_i
-        
 
-        # We pass the predicted word as the next input to the model
-        # along with the previous hidden state
-        input_eval = tf.expand_dims([predicted_id], 0)
+if __name__ == "__main__":
+    # model for predicting the inital state 
+    batch_z = tf.keras.Input(shape=(HP.latent_dim,))
+    initial_state = tf.keras.layers.Dense(units=(2*HP.dec_hidden_size), activation='tanh', name = "decoder_init_stat")(batch_z)
+    latent_to_hidden_state_model = tf.keras.Model(inputs=batch_z, outputs=initial_state)
+    latent_to_hidden_state_model.load_weights("model_weight.h5", by_name = True)
+    latent_to_hidden_state_model.summary()
 
-        text_generated.append(idx2char[predicted_id])
+    # create the LSTM for generating
+    """
+    We have 3 input tensor. The input of the LSTM and the hidden states 
+    """
+    decoder_input = tf.keras.Input(shape=(1, 5 + HP.latent_dim))
+    initial_h_input = tf.keras.Input(shape=(HP.dec_hidden_size,))
+    initial_c_input = tf.keras.Input(shape=(HP.dec_hidden_size,))
+    # now the LSTM
+    decoderLSTM = tf.keras.layers.LSTM(HP.dec_hidden_size, recurrent_dropout=HP.rec_dropout, 
+                                        return_sequences=True, return_state=True, name = "LSTM_decoder")
 
-    return (start_string + ''.join(text_generated))
+    # creation of the LSTM
+    decoder_output, h_new, c_new = decoderLSTM(decoder_input, initial_state = [initial_h_input, initial_c_input])
+    # dense to output. THe dimention is, as explained in the paper equal to 3 + 6*M
+    # 6 times M= number of mixture 
+    output_dimention = (3 + HP.M * 6)
+    distribution_output = tf.keras.layers.Dense(output_dimention, name = "output_layer")(decoder_output)
+
+    # Now we load the weights from the trained model
+    generator = tf.keras.models.Model([decoder_input, initial_h_input, initial_c_input], outputs =[ distribution_output , h_new, c_new])
+    generator.summary()
+    generator.load_weights("model_weight.h5", by_name = True)
+    generator.build(tf.TensorShape([1, None])) 
+
+    seq =  generate_sketch(generator, latent_to_hidden_state_model)
+

src/train.py
@@ -15,7 +15,7 @@ tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="log")
 datas = data_Manager.Data(size = 1000)
 # create the callback for data augmentaion during training
 train_generator = data_Manager.DataGenerator(datas.train)
-valid_generator = data_Manager.DataGenerator(datas.valid, validation = True)
+valid_generator = data_Manager.DataGenerator(datas.valid, validation = True, shuffle = True)
 
 
 """
@@ -94,9 +94,10 @@ validation_encoder = datas.valid
 validation_decoder = data_Manager.create_decoder_input(validation_encoder)
 
 vaidation = [validation_encoder, validation_decoder]
-history = seq_to_seq_VAE.fit_generator(train_generator
-                                ,steps_per_epoch=(datas.trainDimention)/HP.batch_size, 
-                                epochs=HP.epochs, callbacks=[KL_wheight_schedule, tensorboard_callback])
+history = seq_to_seq_VAE.fit_generator(train_generator,
+            validation_data= ([validation_encoder, validation_decoder],[]),
+                        steps_per_epoch=(datas.trainDimention)/HP.batch_size, 
+                        epochs=HP.epochs, callbacks=[KL_wheight_schedule, tensorboard_callback])
 # save the model
 seq_to_seq_VAE.save_weights("model_weight.h5")
 seq_to_seq_VAE.save("model.h5")

src/train_utility.py
@@ -7,6 +7,7 @@ import keras.backend as K
 from HyperParameters import HP
 import math as m
 import sys
+import random
 
 def sampling(args):
     """Reparameterization trick by sampling fr an isotropic unit Gaussian.
@@ -38,8 +39,8 @@ def reconstruction_loss(y_true, output):
     # 2. find posterior for each mixture
     # 2.1 but first find dx, dy, p1, p2, p3 
     [dx, dy] = [y_true[:, :, 0], y_true[:, :, 1]]
-    # we have to stack the value for each 
-    penstates = y_true[:,:,2:5]
+    penstates = y_true[:,:,2:]
+
     mixture_posterior = misturegaussian(dx, dy, mux, muy, sigmax, sigmay, ro)
     # now we can obtgain the likelihood of the gaussian
     # first multiply by the Pi terms
@@ -48,13 +49,13 @@ def reconstruction_loss(y_true, output):
     # the loss due the dx, dy 
     # [100, 200, 1]
     L_s = -tf.math.log(tf.math.reduce_sum(mixture_posterior_weighted, 2, keepdims=True) + epsilon )
-    
+    # create a vector equal to zero where the strokes end
+    zero_after_end = tf.expand_dims(1. - penstates[:,:,2], -1)
+
+    L_s = L_s * zero_after_end
     #############################################
     # Now the loss due to the pen state. Classical cross entropy
-    p_k_dot_log_q_k = tf.math.reduce_sum(penstates*tf.math.log(q + epsilon), axis = 2)
-    L_p = p_k_dot_log_q_k
-    #L_p = tf.losses.categorical_crossentropy(penstates, q)
-    L_p = tf.expand_dims(L_p, -1)
+    L_p = -tf.math.reduce_sum(penstates*tf.math.log(q + epsilon), axis = 2, keepdims=True)
     L_r = L_s + L_p
     L_r = tf.reduce_mean(L_r)
     #L_r = tf.print_tensor(L_r, message='L_r = ')
@@ -89,19 +90,15 @@ def misturegaussian(dx, dy, mux, muy, sigmax, sigmay, ro):
     return mixture_final
         
     
-def find_distribution_parameter(output, temperature = 1):
+def find_distribution_parameter(output):
     # the raw output has to be divided into the the distribution parameters.
     # they also have to be normalized
     # 3 parameters for the pen state 
     logit = output[:,:,:3]
-    logit = logit/temperature
     # now we have 6 parameter for each gaussian
     distribution_parameter = [output[:, :, (3 + HP.M * (n - 1)):(3 + HP.M * n)] for n in range(1, 7)]
     # now divide them 
     [pi, mux, muy, sigmax, sigmay, ro] = distribution_parameter
-    pi = pi/ temperature
-    sigmax = sigmax*temperature
-    sigmay = sigmay*temperature
     # normalize
     # sigma > 0 -> exp(sigma)
     sigmax = tf.math.exp(sigmax)
@@ -113,5 +110,6 @@ def find_distribution_parameter(output, temperature = 1):
     logit = tf.math.softmax(logit)
     return logit, pi, mux, muy, sigmax, sigmay, ro
     
+
 
 